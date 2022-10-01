53b8ae92b373888b91c656f1e13e389ea48259d7
Kenneth Mathari <mathari580@gmail.com>
2022-10-01 06:16:47 +0300

    created data classes

--- app/src/main/java/com/kennethmathari/rickandmorty/network/RickandMortyService.kt
+++ app/src/main/java/com/kennethmathari/rickandmorty/network/RickandMortyService.kt
@@ -1,9 +1,10 @@
 package com.kennethmathari.rickandmorty.network
 
+import com.kennethmathari.rickandmorty.model.Character
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface RickandMortyService {
     @GET("character/2")
-    fun getCharacterbyID(): Call<Any>
+    fun getCharacterbyID(): Call<Character>
 }
--- app/src/main/java/com/kennethmathari/rickandmorty/viewmodel/RickandMortyViewModel.kt
+++ app/src/main/java/com/kennethmathari/rickandmorty/viewmodel/RickandMortyViewModel.kt
@@ -2,12 +2,12 @@ package com.kennethmathari.rickandmorty.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.kennethmathari.rickandmorty.model.Character
 import com.kennethmathari.rickandmorty.network.RetrofitInstance
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
 
-class RickandMortyViewModel: ViewModel() {
+class RickandMortyViewModel : ViewModel() {
 
     // Init block to fetch character by Id when the [RickandMortyViewModel] is initialized
 //    init {
@@ -15,21 +15,22 @@ class RickandMortyViewModel: ViewModel() {
 //    }
 
     fun getCharacterbyId() {
-        RetrofitInstance.rickandMortyService.getCharacterbyID().enqueue(object : retrofit2.Callback<Any> {
+        RetrofitInstance.rickandMortyService.getCharacterbyID()
+            .enqueue(object : retrofit2.Callback<Character> {
 
-            override fun onResponse(call: Call<Any>, response: Response<Any>) {
-                if (response.isSuccessful) {
-                    //val character = response.body()
-                    //Log.d("Characters", "onResponse: $character")
-                    //Log.d("TAG", "onResponse: ${character?.name}")
-                    Log.e("TAG", "onResponse: ${response.body()}")
+                override fun onResponse(call: Call<Character>, response: Response<Character>) {
+                    if (response.isSuccessful) {
+                        //val character = response.body()
+                        //Log.d("Characters", "onResponse: $character")
+                        //Log.d("TAG", "onResponse: ${character?.name}")
+                        Log.e("TAG", "onResponse: ${response.body()}")
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<Any>, t: Throwable) {
-                Log.d("TAG", "onFailure: ${t.message}")
-            }
-        })
+                override fun onFailure(call: Call<Character>, t: Throwable) {
+                    Log.d("TAG", "onFailure: ${t.message}")
+                }
+            })
     }
 }
 
