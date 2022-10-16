8151c5f45114154753a69a3293fc1df456f8d311
Kenneth Mathari <mathari580@gmail.com>
2022-10-16 13:47:49 +0300

    implemented CharacterListPagingEpoxyController

---
+++ app/src/main/java/com/kennethmathari/rickandmorty/views/epoxy/CharacterListPagingEpoxyController.kt
@@ -0,0 +1,28 @@
+package com.kennethmathari.rickandmorty.views.epoxy
+
+import com.airbnb.epoxy.EpoxyModel
+import com.airbnb.epoxy.paging.PagedListEpoxyController
+import com.kennethmathari.rickandmorty.R
+import com.kennethmathari.rickandmorty.data.model.Character
+import com.kennethmathari.rickandmorty.databinding.CharacterItemBinding
+import com.squareup.picasso.Picasso
+
+class CharacterListPagingEpoxyController : PagedListEpoxyController<Character>() {
+    override fun buildItemModel(currentPosition: Int, item: Character?): EpoxyModel<*> {
+        return CharacterGridItemEpoxyModel(
+            imageUrl = item!!.image,
+            name = item.name
+        ).id(item.id)
+    }
+
+    data class CharacterGridItemEpoxyModel(
+        val imageUrl: String,
+        val name: String,
+    ) : ViewBindingKotlinModel<CharacterItemBinding>(R.layout.character_item) {
+        override fun CharacterItemBinding.bind() {
+            Picasso.get().load(imageUrl).into(characterImageView)
+            characterNameTextView.text = name
+        }
+    }
+
+}
