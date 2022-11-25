bb45134e1e9743e46c034e6e0e5915c74f6824d4
Kenneth Mathari <mathari580@gmail.com>
2022-11-25 20:17:52 +0300

    fragments cleanup

---
+++ app/src/main/java/com/kennethmathari/rickandmorty/views/fragments/CharacterDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.kennethmathari.rickandmorty.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CharacterDetailFragment : Fragment() {
+    
+}
