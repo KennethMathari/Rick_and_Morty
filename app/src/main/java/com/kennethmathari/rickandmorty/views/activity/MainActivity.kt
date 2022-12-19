c41b479af93201ea0c234840940e536768a83548
Kenneth Mathari <mathari580@gmail.com>
2022-12-19 21:06:22 +0300

    added drawer layout

--- app/src/main/java/com/kennethmathari/rickandmorty/views/activity/MainActivity.kt
+++ app/src/main/java/com/kennethmathari/rickandmorty/views/activity/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.kennethmathari.rickandmorty.R
 import com.kennethmathari.rickandmorty.databinding.ActivityMainBinding
 
@@ -13,15 +15,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
         setupActionBarWithNavController(navController)
+
+        binding.navView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
