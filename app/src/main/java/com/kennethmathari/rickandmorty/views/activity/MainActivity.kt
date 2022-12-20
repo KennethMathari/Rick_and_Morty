4f03478bd4290e62ea13f70cef788180fe03d61c
Kenneth Mathari <mathari580@gmail.com>
2022-12-20 08:17:08 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kennethmathari/rickandmorty/views/activity/MainActivity.kt
+++ app/src/main/java/com/kennethmathari/rickandmorty/views/activity/MainActivity.kt
@@ -3,9 +3,10 @@ package com.kennethmathari.rickandmorty.views.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.kennethmathari.rickandmorty.R
@@ -14,6 +15,7 @@ import com.kennethmathari.rickandmorty.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,17 +23,19 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.findNavController()
-        setupActionBarWithNavController(navController)
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.navView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        navController = findNavController(this,R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
