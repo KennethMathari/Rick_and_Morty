c2f00f9cb517675f3b57fef2dd932f5824057d10
Kenneth Mathari <mathari580@gmail.com>
2022-12-20 09:00:58 +0300

    created menu items for drawer layout

---
+++ app/src/main/java/com/kennethmathari/rickandmorty/views/fragments/EpisodeListFragment.kt
@@ -0,0 +1,28 @@
+package com.kennethmathari.rickandmorty.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kennethmathari.rickandmorty.R
+import com.kennethmathari.rickandmorty.databinding.FragmentEpisodeListBinding
+
+
+class EpisodeListFragment : Fragment(R.layout.fragment_episode_list) {
+
+    private var _fragmentEpisodeListBinding: FragmentEpisodeListBinding? = null
+    private val fragmentEpisodeListBinding get() = _fragmentEpisodeListBinding
+    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _fragmentEpisodeListBinding = FragmentEpisodeListBinding.bind(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _fragmentEpisodeListBinding = null
+    }
+
+}
